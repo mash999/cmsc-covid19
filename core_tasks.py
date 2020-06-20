89861588871dcac6d4244e8fd59bacbf8c9bd589
Dewan Ruhul Mushfiq <drmushfiq@mun.ca>
2020-06-19 22:47:02 -0230

    automating getting csv data into pandas dataframe in py file

--- core_tasks.py
+++ core_tasks.py
@@ -5,3 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.dates as mdates
+
+#Download csv file:
+#Getting csv data into a pandas dataframe
+data = pd.read_csv('https://health-infobase.canada.ca/src/data/covidLive/covid19.csv',sep=',')
